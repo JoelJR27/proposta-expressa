da14d24cbe9d0fb4e0fb20c5e0625781acfd942c
Joel Júnior <joelviana250@gmail.com>
2025-07-02 19:58:04 -0300

    commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <main className="min-h-screen w-full bg-background">
+      <Header />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return(
+        <header className="bg-white p-5 text-xs border-border border xs:text-sm sm:text-base md:flex md:gap-5 md:items-center lg:text-lg">
+            <h1 className="text-2xl font-bold font-primary text-primary-color pb-1">PropostaExpressa</h1>
+            <h2 className="font-times italic text-secondary-text">Gere propostas profissionais em minutos, direto do navegador.</h2>
+        </header>
+    )
+}
