3d15a64d9b7a75b31102b4d18b6d09f89ddf5fdb
Joel Júnior <joelviana250@gmail.com>
2025-07-16 14:14:16 -0300

    style: alterar cor do 'add button' e fonte

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,13 +5,13 @@ type ButtonProps = {
 };
 export function Button({ children, buttonType, actions }: ButtonProps) {
   const buttonTypeOptions = {
-    add: "primary-blue",
+    add: "secondary-blue",
     remove: "red-500",
     export: "secondary-blue",
   };
   return (
     <button
-      className={`text-${buttonTypeOptions[buttonType]} font-medium p-2 cursor-pointer max-xs:text-[10px]`}
+      className={`text-${buttonTypeOptions[buttonType]} font-secondary font-medium p-2 cursor-pointer max-xs:text-[10px]`}
       onClick={actions}
     >
       {children}
