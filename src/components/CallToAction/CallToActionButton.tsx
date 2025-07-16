4ffb4ebf14cfa0c1202dc40ddd4b3bbf5ac09a3c
Joel Júnior <joelviana250@gmail.com>
2025-07-16 14:16:02 -0300

    style: alterar o peso da fonte do botão

--- src/components/CallToAction/CallToActionButton.tsx
+++ src/components/CallToAction/CallToActionButton.tsx
@@ -7,7 +7,7 @@ type CallToActionButtonProps = {
 export function CallToActionButton({ children, scrollToRef }: CallToActionButtonProps) {
   return (
     <button
-      className="p-3 bg-primary-blue w-fit font-primary text-base text-white text-center text-pretty rounded-default-radius hover:bg-secondary-blue hover:rotate-[-4deg] transition-all delay-[30ms] cursor-pointer"
+      className="p-3 bg-primary-blue w-fit font-primary font-medium text-base text-white text-center text-pretty rounded-default-radius hover:bg-secondary-blue hover:rotate-[-4deg] transition-all delay-[30ms] cursor-pointer"
       onClick={(event) => {
         event.preventDefault();
         handleScrollTo(scrollToRef);
